ba12d3ee98d9f173398f91d6b7fbcc2d774bf47c
OlleEatsBulle <56720085+OlleEatsBulle@users.noreply.github.com>
2019-11-26 11:05:27 +0100

    uppgifter

---
+++ uppgifterna/src/User.java
@@ -0,0 +1,35 @@
+public class User {
+
+    private String Namn;
+    private String Persnr;
+    private int Timlön;
+    private int Timmar;
+
+
+    public String getNamn() {
+        return Namn;
+    }
+
+    public void setNamn(String N) {
+        Namn = N;
+    }
+    public String getPersnr() {
+        return Persnr;
+    }
+    public void setPersnr(String Pn) {
+        Persnr = Pn;
+    }
+    public int getTimlön(){
+        return Timlön;
+    }
+    public void setTimlön(int Tl){
+        Timlön = Tl;
+    }
+    public int getTimmar(){
+        return Timmar;
+    }
+    public void setTimmar(int T){
+        Timmar = T;
+    }
+
+}
--- uppgifterna/src/uppg4_1.java
+++ uppgifterna/src/uppg4_1.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+
 
 public class uppg4_1 {
     public static void main (String []args){
-       Scanner s = new Scanner (System.in);
 
-            String Namn;
-            String persnr;
-            int timlön;
-            int timmar;
+        User user = new User();
+
+        user.setNamn("Namn:");
+        user.setPersnr("Persnr:");
+
+
+        System.out.println(user.getNamn());
+        System.out.println(user.getPersnr());
+
         }
     }
 
